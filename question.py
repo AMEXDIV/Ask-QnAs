c3da16867986665486d70830d4fc35497320cf9c
Divyansh Bhati <87819841+AMEXDIV@users.noreply.github.com>
2021-09-19 22:40:57 +0530

    question.py

---
+++ question.py
@@ -0,0 +1,22 @@
+# Let items be MacMini , iPhone SE ,  OPPO RENO 6 , AirPods , SSDs 512GB
+items = ["MacAir" , "iPhone SE" ,  "OPPO RENO 6" , "AirPods" , "SSDs 512GB"]
+
+S1  = pd.Series(data=[43000,8100,16000,65500,6000], index=items)
+S2  = pd.Series(data=[40000,8900,14000,34000,9000], index=items)
+S3  = pd.Series(data=[63000,8800,18000,92500,7800], index=items)
+S4  = pd.Series(data=[69000,7600,34000,62500,9500], index=items)
+S5  = pd.Series(data=[76000,5500,87000,87500,9000], index=items)
+S6  = pd.Series(data=[53000,5200,89000,78500,6100], index=items)
+S7  = pd.Series(data=[97000,4500,10000,36500,3200], index=items)
+S8  = pd.Series(data=[94000,5100,64000,32000,6500], index=items)
+S9  = pd.Series(data=[24000,1100,67000,69800,1800], index=items)
+S10 = pd.Series(data=[56000,2200,98000,88900,1000], index=items)
+S11 = pd.Series(data=[79000,4300,21000,97600,7900], index=items)
+S12 = pd.Series(data=[56000,5400,11000,13000,6500], index=items)
+
+"""
+SUMITA ARORA 
+informatics-practices 
+page 84 
+question 6
+"""
